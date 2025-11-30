52f1f76e812f2b7e3d6d4c192acc7cb78b361fab
천태웅 <ctw0727@naver.com>
2025-11-30 18:25:14 +0900

    Update window.h

--- window.h
+++ window.h
@@ -10,6 +10,8 @@
 
 void clrscr();
 void delay(int);
+void disable_raw_mode();
+void enable_raw_mode();
 
 void clrscr() {
     system("cls");
@@ -19,5 +21,13 @@ void clrscr() {
 // 밀리초 단위로 동작
 void delay(int t){
 	Sleep(t);
+	return;
+}
+
+void disable_raw_mode(){
+	return;
+}
+
+void enable_raw_mode(){
 	return;
 }
